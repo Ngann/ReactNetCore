2e92a97907a505c598984937db89dfa3d1e7acc2
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-28 14:19:39 -0700

    add Nunit testing to project

---
+++ ReactTemplate/Controllers/PrimeServiceController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactTemplate.Controllers
+{
+    public class PrimeServiceController : Controller
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+
+        public static bool isMagicSquare(List<List<decimal>> matrix)
+        {
+            return true;
+        }
+    }
+}
---
+++ ReactTemplateTest/PrimeServiceControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ReactTemplate.Controllers
+{
+    [TestFixture]
+    public class PrimeServiceControllerTests
+    {
+        [TestFixture]
+        public class PrimeService_IsPrimeShould
+        {
+            [Test]
+            public void IsPrime_InputIs1_ReturnFalse()
+            {
+                using (PrimeServiceController primeService = CreatePrimeService())
+                {
+                    var result = primeService.IsPrime(1);
+                    Assert.IsFalse(result, "1 should not be prime");
+                }
+
+            }
+
+            [TestCase(-1)]
+            [TestCase(0)]
+            [TestCase(1)]
+            public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+            {
+                using (PrimeServiceController primeService = CreatePrimeService())
+                {
+                    var result = primeService.IsPrime(value);
+                    Assert.IsTrue(result, $"{value} should not be prime");
+                }
+
+            }
+
+            [Test]
+            public void When_MatrixIsLessThanThreeByThree_Then_IsMagicSquareFalse()
+            {
+                var matrix = new List<List<decimal>>
+               {
+                   new List<decimal> {1.0m, 2.0m},
+                   new List<decimal> {4.0m, 4.0m}
+               };
+
+                Assert.IsTrue(PrimeServiceController.isMagicSquare(matrix));
+            }
+
+            private PrimeServiceController CreatePrimeService()
+            {
+                return new PrimeServiceController();
+            }
+        }
+    }
+}
+
+//dotnet add package Microsoft.AspNetCore.Mvc.Abstractions --version 2.2.0
+//dotnet add package Microsoft.AspNetCore.Mvc.ViewFeatures --version 2.2.0
