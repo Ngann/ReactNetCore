000ff1c75f45027e31d5aab4de445d20ae352646
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-29 12:31:09 -0700

    update readme for VS Code

--- ReactTemplate/Models/MvcMovieContext.cs
+++ ReactTemplate/Models/MvcMovieContext.cs
@@ -5,12 +5,12 @@ namespace ReactTemplate.Models
 {
     public class MvcMovieContext : DbContext
     {
-        //public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
-        //    : base(options)
-        //{
-        //}
+		public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+			: base(options)
+		{
+		}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Database=my_d;Host=localhost;Port=5432");
 
         public DbSet<Movie> Movie { get; set; }
